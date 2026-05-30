a1a7b5a6de2d9215ccb3872fd32f38dc432af240
Ian Whitcomb <ian@bartr.co>
2026-05-30 10:38:10 -0500

    fix(config): update local URLs and modify ecosystem configuration for Caddy server

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,19 +2,11 @@ module.exports = {
     apps: [
       {
         name: "bartr-site",
-        script: "bundle",
-        args: "exec jekyll serve",
-        interpreter: "none",
-        env_local: {
-          ENABLED: "true",
-        },
-        env_development: {
-          ENABLED: "false",
-        },
-        env_production: {
-          ENABLED: "false",
-        },
-      }
+        script: "caddy",
+        args:
+          "run --config ./Caddyfile",
+        exec_mode: "fork",
+      },
     ],
   };
   
